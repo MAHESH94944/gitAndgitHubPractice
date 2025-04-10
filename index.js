dba4444ca7a3723921f11413908ca5a78f693648
ADITYA MUNJALE <munjaleaditya13@gmail.com>
2025-04-10 15:19:48 +0530

    added style.css folder

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function add(a, b) {
   return a + b;
 }
-<<<<<<< HEAD
 
 function mul(a, b) {
   return a * b;
 }
-=======
-console.log("hello world");
->>>>>>> 6dbe78e2fb0e6031ec78257c09dc1c274e8f83c6
+
+
+
+
